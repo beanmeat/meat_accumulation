0953c260b1709087e6629cae31b2e65fa64b65f9
3090018282@qq.com <3090018282@qq.com>
2025-03-22 19:28:33 +0800

    AliPay

---
+++ alipay_demo/src/main/java/pay/PayApplication.java
@@ -0,0 +1,15 @@
+package pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+}
---
+++ alipay_demo/src/main/java/pay/alipay/entity/OrderStateEntity.java
@@ -0,0 +1,27 @@
+package pay.alipay.entity;
+
+public class OrderStateEntity {
+        private int code;
+        private String msg;
+
+        public OrderStateEntity(int code, String msg) {
+            this.code = code;
+            this.msg = msg;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+    }
